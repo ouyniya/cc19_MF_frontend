eeb272f8ddc3e5c92bd07e94ca62527e1ee0c091
niya-s <niya.somkerd@gmail.com>
2025-05-17 12:18:26 +0700

    fix: login session expired

--- src/routes/ProtectAuth.jsx
+++ src/routes/ProtectAuth.jsx
@@ -1,16 +1,21 @@
-import { useEffect, useState } from "react"; 
+import { useEffect, useState } from "react";
 import useUserStore from "../stores/useUserStore";
 import { Loader } from "lucide-react";
 import { Link } from "react-router";
 
 function ProtectAuth(props) {
-  const { el } = props; 
+  const { el } = props;
 
   // get user and token from store
   const token = useUserStore((state) => state.token);
-  const user = useUserStore((state) => state.user);
+  const getCurrentUser = useUserStore((state) => state.getCurrentUser);
+
+  //loading
+  const [loading, setLoading] = useState(true);
+
   // set status login >> ok or not?
   const [ok, setOk] = useState(null);
+  const [tokenExpired, setTokenExpired] = useState(null);
 
   useEffect(() => {
     checkPermission();
@@ -20,23 +25,54 @@ function ProtectAuth(props) {
   const checkPermission = async () => {
     try {
       // get user data from backend
-      const id = user?.id;
-      // console.log(id)
-      if (!id) {
-        setOk(true);
+      const user = await getCurrentUser(token);
+
+      console.log("user", user);
+
+      if (user) {
+        setOk(false);
       }
     } catch (error) {
-      setOk(false);
+      const errorMsg = error?.response?.data?.message;
+      console.log(errorMsg);
+
+      if (errorMsg === "jwt expired") {
+        setTokenExpired(true);
+      }
+
+      setOk(true);
     }
   };
 
+  if (tokenExpired) {
+    return (
+      <>
+        <div className="flex w-screen justify-center items-center">
+          <span className="text-center">
+            Log in หมดอายุแล้ว กรุณา Log in ใหม่อีกครั้ง
+          </span>
+        </div>
+        {el}
+      </>
+    );
+  }
+
+  // loading part
+  if (ok === null) {
+    return <>
+    <div className="w-screen h-screen flex justify-center items-center">
+    <Loader className="animate-spin" />
+    </div>
+    </>
+  }
 
   if (!ok) {
     return (
       <>
-      <div className="text-center text-xl font-bold py-[350px]">
-      คุณได้ Log in แล้ว
-      </div></>
+        <div className="text-center text-xl font-bold py-[350px]">
+          คุณได้ Log in แล้ว
+        </div>
+      </>
     );
   }
   return <>{el}</>; // el คือ Component ที่จะเรนเดอร์ถ้าผู้ใช้มีสิทธิ์
